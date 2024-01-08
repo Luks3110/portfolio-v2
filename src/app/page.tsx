ce5b4be81dd0cd41e4c23c61a865e5580dfad49c
Lucas Fernandes <lucas.pereira@arboimoveis.com>
2024-01-07 22:08:49 -0300

    fix: added skills to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import About from "./components/about";
 import Hero from "./components/hero";
 import Projects from "./components/projects";
+import Skills from "./components/skills";
 import Divider from "./components/ui/divider";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Divider />
       <About />
       <Projects />
+      <Skills />
     </main>
   );
 }
